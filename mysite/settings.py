a09ad06abac4c1aeb2b0ef09afc804d3f945c442
sarveshgandhi <42963045+sarveshgandhi@users.noreply.github.com>
2023-05-14 23:25:09 +0530

    database changed to postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,8 +139,8 @@ if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
 if os.getenv("TRAMPOLINE_CI", None):
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgresql",
         }
     }
 
